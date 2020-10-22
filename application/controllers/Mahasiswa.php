268aa7804745d0003b90fa0e26c4a49c958eb780
Pahrul Irfan <ip.5090@gmail.com>
2020-10-22 16:46:56 +0800

    update controller mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -4,6 +4,12 @@ class Mahasiswa extends CI_Controller
 {
 	public function index()
 	{
+		// Load dari folder view
 		$this->load->view('mahasiswa/v_index');
+	}
+
+	public function create()
+	{
+		$this->load->view('mahasiswa/v_create');
 	}
 }
